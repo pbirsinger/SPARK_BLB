e7a705db184d78e4e7ea9342301494503d2855e8
pbirsinger <pbirsinger@berkeley.edu>
2012-11-02 14:45:10 -0700

    merged blb.py changes

--- blb.py
+++ blb.py
@@ -1,35 +1,36 @@
+"""
+Main class representing the BLB algorithm.
 
+"""
+
+import random
+import numpy
+import asp.config
+import inspect, ast
+from convert.blb_convert import BLBConverter, create_data_model
+import convert.blb_convert_data_model as data_model
+from blb_setup import gslroot, cache_dir
 import asp.jit.asp_module as asp_module
 import asp.avro_inter.avro_backend as avro_backend    
 import asp.codegen.ast_tools as ast_tools
 from asp.codegen.codegen_scala import *
 import ast
 
-import random
-import numpy
-import asp.config
-import inspect, ast
-
-#from blb_convert import BLBConverter
-#from blb_setup import gslroot, cache_dir
-
 def combine(blb_funcs):
     parent = (open('blb_core_parallel.scala')).read()  
     return parent + blb_funcs 
-
+	
 class BLB:
     known_reducers= ['mean', 'stdev', 'mean_norm', 'noop']
     def __init__(self, num_subsamples=25, num_bootstraps=100, 
-                 subsample_len_exp=0.6, with_cilk=False, with_openMP=False,
-                 dimension=1, pure_python=False, use_scala=False):
+                 subsample_len_exp=0.5, with_cilk=False, with_openMP=False,
+	   	 dimension=1, pure_python=False, use_scala=False):
 
         self.dim = dimension
         self.with_cilk=with_cilk
-        self.with_openMP = with_openMP
-        self.pure_python= pure_python
-        self.use_scala = use_scala
+	self.with_openMP = with_openMP
+	self.pure_python= pure_python
 
-        
         self.estimate_src = inspect.getsource(self.compute_estimate)
         self.estimate_ast = ast.parse(self.estimate_src.lstrip())
 
@@ -45,40 +46,39 @@ class BLB:
         self.cached_mods = {}
     
     def fingerprint(self, data):
-        """
+	"""
         Return a tuple of problem information sufficient to
         determine compilation-equivalence.
         """
         return tuple( [ (datum.shape,datum.dtype) for datum in data ] )
-          
+
     def run(self, *data):
         if self.pure_python:
-            self.run_python(data)
-        elif self.use_scala: 
-            self.run_distributed(data)
-        else: #run local c++ 
+            subsample_estimates = []
+            for i in range(self.num_subsamples):
+                subsample = self.__subsample(data, self.subsample_len_exp)
+                bootstrap_estimates = [] 
+                for j in range(self.num_bootstraps):
+                    bootstrap = self.__bootstrap(subsample)
+                    estimate = self.compute_estimate(bootstrap)
+                    bootstrap_estimates.append(estimate)
+#                    print "***PYTHON bootstrap estimate for bootstrap " + str(j) + " and subsample " + str(i) + " is " + str(estimate)
+                subsample_est = self.reduce_bootstraps(bootstrap_estimates)
+                subsample_estimates.append(subsample_est)
+#                print "***PYTHON subsample estimate for subsample " + str(i) + " is " + str(subsample_est)
+            return self.average(subsample_estimates)
+	elif self.use_scala:
+	    self.run_distributed(data)
+        else:
             f = self.fingerprint(data)
             mod = None
             if f in self.cached_mods:
                 mod = self.cached_mods[f]
-            else:   
+            else:
                 mod = self.build_mod(f)
                 self.cached_mods[f] = mod
-            return mod.compute_blb(data)
-        
-    def run_python(self,data):
-        subsample_estimates = []
-        for i in range(self.num_subsamples):
-            subsample = self.__subsample(data, self.subsample_len_exp)
-            bootstrap_estimates = [] 
-            for j in range(self.num_bootstraps):
-                bootstrap = self.__bootstrap(subsample)
-                estimate = self.compute_estimate(bootstrap)
-                bootstrap_estimates.append(estimate)
-            subsample_est = self.reduce_bootstraps(bootstrap_estimates)
-            subsample_estimates.append(subsample_est)
-        return self.average(subsample_estimates)
-    
+            return mod.compute_blb(*data)
+
     def run_distributed(self,data): 
         mod = asp_module.ASPModule(cache_dir = "/root/spark/examples/target/scala-2.9.1.final/classes/", use_scala=True)
                                                                   
@@ -104,16 +104,33 @@ class BLB:
         model_filename = data[1]
         return mod.run_outer(email_filename, model_filename, self.dim, self.num_subsamples, self.num_bootstraps, self.subsample_len_exp)  
  
-    def compile_for( self, data, key=None ):
-        f = key if key else self.fingerprint(data)
-        mod = None
-        if f in self.cached_mods:
-            mod = self.cached_mods[f]
-        else:
-            mod = self.build_mod(f)
-            mod.backends["c++"].compile()
-            self.cached_mods[f] = mod
+    def compile_for( self, key  ):
+	mod = None
+	if key in self.cached_mods:
+	    mod = self.cached_mods[key]
+	else:
+	    mod = self.build_mod(key)
+	    mod.backends["c++"].compile()
+	    self.cached_mods[key] = mod
 
+    def create_input_model( self, key, vec_n ):
+	#Subsample and load cutoff size
+	cutoff = 10*1024*1024
+	models = []
+	for k in key:
+	    print k
+	    size = reduce( int.__mul__, k[0][1:] ) if len(k[0]) > 1 else 1
+	    size *= vec_n
+	    model = None
+	    if size <= cutoff:
+		model = data_model.DataModel( k[1], list(k[0]) ) 
+	    else:
+		model = data_model.IndirectDataModel( k[1], list(k[0]) )
+	    model.set_len( k[0][0] )
+	    model.dimensions[0] = vec_n
+	    models.append( model )
+	return models
+	    
     def build_mod(self, key):
         template_name = ''
         if self.with_openMP:
@@ -128,44 +145,55 @@ class BLB:
         blb_template = template.Template(filename="templates/%s" % template_name, disable_unicode=True)
         impl_template = template.Template(filename="templates/blb_impl.mako", disable_unicode=True)
 
-            
-        if key[0] % self.dim != 0:
-            raise ValueError( 'Data must be of dimension %d' % self.dim )
-        n_vecs = key[0] / self.dim
-        vec_n = int( pow( n_vecs, self.subsample_len_exp ) )
-        impl_args = {'dim': self.dim}
-        impl_args['n_data'] = key[0]
-        impl_args['sub_n'] = int( pow( key[0], self.subsample_len_exp ) )
+        #leading dimension of first data object tuple 
+	n_vecs = key[0][0][0]
+
+	vec_n = int( pow( n_vecs, self.subsample_len_exp ) )
+	impl_args = {}
+        #impl_args = {'dim': self.dim}
+        #impl_args['n_data'] = key[0]
+        #impl_args['sub_n'] = int( pow( key[0], self.subsample_len_exp ) )
         impl_args['vec_n'] = vec_n
-        impl_args['n_vecs'] = n_vecs 
-        impl_funcs = []
-        impl_args['scalar_type'] = 'double'
-    
-        estimate_converter = BLBConverter( key, input_size=vec_n, weighted=True )
-        estimate_cpp = estimate_converter.render( self.estimate_ast )
-        impl_args['classifier'] = estimate_cpp
-        impl_funcs.extend( estimate_converter.desired_funcs )
-        impl_args['bootstrap_dim'] = estimate_converter.output_dim()
-    
-        reduce_converter = BLBConverter( impl_args['bootstrap_dim'], False )
-        reduce_cpp = reduce_converter.render( self.reduce_ast )
-        impl_args['bootstrap_reducer'] = reduce_cpp
-        impl_funcs.extend( reduce_converter.desired_funcs )
-        impl_args['subsample_dim'] = reduce_converter.output_dim()
-    
-        average_converter = BLBConverter( impl_args['subsample_dim'], False )
-        average_cpp = average_converter.render( self.average_ast )
-        impl_args['subsample_reducer'] = average_cpp
-        impl_funcs.extend( average_converter.desired_funcs )
-        impl_args['average_dim'] = average_converter.output_dim()
-    
-        impl_args['desired_funcs'] = set(impl_funcs)
-        fwk_args = self.set_framework_args(key, impl_args.copy())
+	impl_args['n_vecs'] = n_vecs 
+	impl_funcs = []
+	impl_args['scalar_type'] = 'double'
+
+
+	estimate_converter = BLBConverter( self.create_input_model(key, vec_n), input_size=vec_n, weighted=True )
+	estimate_cpp = estimate_converter.render( self.estimate_ast )
+	impl_args['classifier'] = estimate_cpp
+	impl_funcs.extend( estimate_converter.desired_funcs )
+ 	impl_args['bootstrap_model'] = estimate_converter.get_ret_model()
+	arg_model = [ model.clone() for model in estimate_converter.arg_model ]
+	for i in range(len(arg_model)):
+	    arg_model[i].dimensions = key[i][0]
+	impl_args['arg_model'] = arg_model
+
+	reduce_input = estimate_converter.get_ret_model().clone()
+	reduce_input.dimensions.insert( 0, self.num_bootstraps )
+	reduce_input.set_len( self.num_bootstraps )
+	reduce_converter = BLBConverter( [reduce_input], input_size=self.num_bootstraps )
+	reduce_cpp = reduce_converter.render( self.reduce_ast )
+	impl_args['bootstrap_reducer'] = reduce_cpp
+	impl_funcs.extend( reduce_converter.desired_funcs )
+	impl_args['subsample_model'] = reduce_converter.get_ret_model()
+
+	average_input = reduce_converter.get_ret_model().clone()
+	average_input.dimensions.insert( 0, self.num_subsamples )
+	average_input.set_len( self.num_subsamples )
+	average_converter = BLBConverter( [average_input], input_size=self.num_subsamples )
+	average_cpp = average_converter.render( self.average_ast )
+	impl_args['subsample_reducer'] = average_cpp
+	impl_funcs.extend( average_converter.desired_funcs )
+	impl_args['average_model'] = average_converter.get_ret_model()
+
+	impl_args['desired_funcs'] = set(impl_funcs)
+	fwk_args = self.set_framework_args(key, impl_args.copy())
         rendered = blb_template.render( **fwk_args )
-    
-    
-        """
-        === This was for non sejitizing code ===
+
+
+	"""
+	=== This was for non sejitizing code ===
         if self.compute_estimate in BLB.known_reducers:
             impl_args['use_classifier'] = self.compute_estimate
         else:
@@ -180,7 +208,7 @@ class BLB:
             impl_args['use_subsample_reducer'] = self.average
         else:
             impl_args['subsample_reducer'] = self.average
-        """
+	"""
 
         rendered_impl = impl_template.render( **impl_args )
         
@@ -214,26 +242,27 @@ class BLB:
         return flat
         
     def set_includes(self, mod):
-        mod.add_header('stdlib.h')
-        mod.add_header('math.h')
-        mod.add_header('time.h')
-        mod.add_header('numpy/ndarrayobject.h')
-        mod.add_header('gsl_rng.h')
-        mod.add_header('gsl_randist.h')
-        mod.add_library( 'blas', [], libraries=['blas'] )
-        mod.add_header('gsl_vector.h')
-        mod.add_header('gsl_matrix.h')
-        mod.add_header('gsl_blas.h')
-        mod.add_header('gsl_linalg.h')
-        mod.add_library( 'cblas', [], libraries=['cblas'] )
-        mod.add_library( 'gsl', [gslroot, gslroot+'/randist', gslroot+'/rng', gslroot+'/matrix', gslroot+'/vector', gslroot+'/blas', gslroot+'/linalg'],
-                         [gslroot+'/.libs'], ['gsl'] )
-        if self.with_cilk:
-            mod.add_header('cilk/cilk.h')
-            mod.add_header('cilk/cilk_api.h')
-        elif self.with_openMP:
-            mod.add_header('omp.h')
-        mod.add_to_init('import_array();')
+	    mod.add_header('stdlib.h')
+            mod.add_header('math.h')
+            mod.add_header('time.h')
+            mod.add_header('numpy/ndarrayobject.h')
+	    mod.add_header('gsl_rng.h')
+	    mod.add_header('gsl_randist.h')
+	    mod.add_library( 'blas', [], libraries=['blas'] )
+	    mod.add_header('gsl_vector.h')
+	    mod.add_header('gsl_matrix.h')
+	    mod.add_header('gsl_blas.h')
+	    mod.add_header('gsl_linalg.h')
+#	    mod.add_library( 'cblas', [], libraries=['cblas'] )
+            mod.add_library( 'gsl', [gslroot, gslroot+'/randist', gslroot+'/rng', gslroot+'/matrix', gslroot+'/vector', gslroot+'/blas', gslroot+'/linalg'],
+				[gslroot+'/.libs'], ['gsl'] )
+            if self.with_cilk:
+                mod.add_header('cilk/cilk.h')
+		mod.add_header('cilk/cilk_api.h')
+            elif self.with_openMP:
+                mod.add_header('omp.h')
+	
+            mod.add_to_init('import_array();')
 
     def set_compiler_flags(self, mod):
         import asp.config
@@ -262,29 +291,25 @@ class BLB:
         Return a dictionary containing the appropriate kwargs for redering
         the framework template.
 
-        the key argument is a fingerprint key for this specialiser
+	the key argument is a fingerprint key for this specialiser
         '''
-        platform = asp.config.PlatformDetector()
-        # estimate cache line size
-        if key[1] is list:
-            vars['seq_type'] = 'list'
-        elif key[1] is numpy.ndarray:
-            vars['seq_type'] = 'ndarray'
-    
+	n_vecs = key[0][0][0]
+	models = create_data_model( key, int( n_vecs**self.subsample_len_exp ) )
+	vars['seq_type'] = 'ndarray'
+#	vars['arg_model'] = models
         vars['n_subsamples'] = self.num_subsamples
         vars['n_bootstraps'] = self.num_bootstraps
-    
+
         if self.with_openMP:
             # specialise this somehow.
-            vars['parallel_loop'] = 'outer'
-            vars['omp_n_threads'] = getattr(self, 'omp_n_threads', 1 )
+	    vars['parallel_loop'] = 'outer'
+	    vars['omp_n_threads'] = getattr(self, 'omp_n_threads', 1 )
         elif self.with_cilk:
             vars['cilk_n_workers'] = getattr(self, 'cilk_n_workers', 1)
-            print 'DEBUG: cilk_nworkers = %d' % vars['cilk_n_workers']
-            vars['parallel_loop'] = 'outer'
+	    print 'DEBUG: cilk_nworkers = %d' % vars['cilk_n_workers']
+	    vars['parallel_loop'] = 'outer'
             
         return vars
-    
     # These three methods are to be implemented by subclasses
     #def compute_estimate(self, sample):
     #    '''The actual statistic being computed. E.g. mean, standard deviation,
